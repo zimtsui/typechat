2ec7528cd97669c307006c38040f42ae0c006d8e
Zim <zimtsui@gmail.com>
2026-03-31 17:46:14 +0800

    exporter

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,4 +1,4 @@
-import { Channel, Exporter } from '@zimtsui/typelemetry/log';
+import { Channel } from '@zimtsui/typelemetry/log';
 import * as Presets from '@zimtsui/typelemetry/log/presets';
 import { formatWithOptions } from 'node:util';
 import { stderr } from 'node:process';
@@ -7,19 +7,19 @@ import { stderr } from 'node:process';
 export const logger = {
     inference: Channel.create<typeof Presets.Level, string>(
         Presets.Level,
-        (chunk: string, level) => Exporter.getGlobalExporter().stream({
+        (chunk: string, level) => Presets.Exporter.getGlobalExporter().stream({
             scope: '@zimtsui/typechat',
             channel: 'Inference',
-            level: Presets.Level[level],
+            level,
             payload: chunk,
         }),
     ),
     message: Channel.create<typeof Presets.Level, unknown>(
         Presets.Level,
-        (payload: unknown, level) => Exporter.getGlobalExporter().monolith({
+        (payload: unknown, level) => Presets.Exporter.getGlobalExporter().monolith({
             scope: '@zimtsui/typechat',
             channel: 'Message',
-            level: Presets.Level[level],
+            level,
             payload: formatWithOptions({ depth: null, colors: !!stderr.isTTY }, payload)
         }),
     ),
