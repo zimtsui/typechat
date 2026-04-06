37015a94caebebbf18c22cda062fb44a350a1995
Zim <zimtsui@gmail.com>
2026-04-06 12:33:28 +0800

    aborterror

--- src/engine.ts
+++ src/engine.ts
@@ -101,7 +101,6 @@ export namespace Engine {
         }
 
         /**
-        * @throws {@link UserAbortion} 用户中止
         * @throws {@link InferenceTimeout} 推理超时
         * @throws {@link ResponseInvalid} 模型抽风
         * @throws {@link NetworkError} 网络故障
@@ -123,8 +122,7 @@ export namespace Engine {
                     if (await validate(response)) return response;
                     else throw new CustomRetry(undefined, { cause: response });
                 } catch (e) {
-                    if (wfctx.signal?.aborted) throw new UserAbortion();                                // 用户中止
-                    else if (signalTimeout?.aborted) e = new InferenceTimeout(undefined, { cause: e }); // 推理超时
+                    if (signalTimeout?.aborted) e = new InferenceTimeout(undefined, { cause: e });      // 推理超时
                     else if (e instanceof ResponseInvalid) {}			                                // 模型抽风
                     else if (e instanceof NetworkError) {}         		                                // 网络故障
                     else if (e instanceof CustomRetry) {}         		                                // 自定义重试
@@ -180,7 +178,6 @@ export namespace Engine {
 }
 
 export class ResponseInvalid extends Error {}
-export class UserAbortion extends Error {}
 export class InferenceTimeout extends Error {}
 export class NetworkError extends Error {}
 export class CustomRetry extends Error {}
